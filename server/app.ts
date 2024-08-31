d0438df8cb4bc21435f62acf2d411d107209a42b
Can <can.bozkurt.dhf@gmail.com>
2024-08-31 12:50:51 +0300

    small apis

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,16 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { expenseRoute } from "./routes/expenses";
 
 const app = new Hono()
 
+app.use("*",logger())
 
 app.get("/", c => {
   return c.body("hi oc")
 })
 
+app.route("/api/expenses", expenseRoute)
+
 export default app
+
---
+++ server/routes/expenses.ts
@@ -0,0 +1,58 @@
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { z } from "zod";
+
+export const expenseRoute = new Hono();
+
+
+const expenseSchema = z.object({
+  id: z.number().int().min(1),
+  title: z.string().min(1, "Title is required").max(50),
+  amount: z.number().positive()
+})
+
+const createExpenseSchema = expenseSchema.omit({ id: true })
+
+type Expense = z.infer<typeof expenseSchema>
+
+const fakeExpenses: Expense[] = [
+  {
+    id: 0,
+    title: "Anan",
+    amount: 1999
+  },
+  {
+    id: 1,
+    title: "Bacın",
+    amount: 2999
+  }
+]
+
+
+expenseRoute.
+get("/", c => { 
+  return c.json(fakeExpenses)
+})
+.get("/:id{[0-9]+}", c => {
+  const id = Number.parseInt(c.req.param('id'))
+
+  const expense = fakeExpenses.find((expense) => expense.id === id)
+
+  if(!expense) {
+    return c.notFound();
+  }
+
+  return c.json(expense)
+
+})
+
+.post("/", zValidator("json", createExpenseSchema), c => { 
+  const expense = c.req.valid("json")
+  fakeExpenses.push({
+    ...expense,
+    id: fakeExpenses.length + 1
+  })
+
+  return c.json(expense)
+})
+
